4dbde02bc9820a03ed450f399360fa669c6a848a
Elena <lenagalkovskaya@yandex.ru>
2022-11-24 13:02:11 +0300

    commit task19

---
+++ task19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+using static System.Console;
+Clear();
+
+string N = (ReadLine()); 
+
+
+
+if ((N[0].ToString(),N[1].ToString()) == (N[4].ToString(),N[3].ToString()))
+{
+   WriteLine("Да"); 
+}
+else
+{
+    WriteLine("Нет");
+}
+
+
